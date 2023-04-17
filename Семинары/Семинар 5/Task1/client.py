f4d0ee4a78ba4370a6f4f493ba1a2e23fc9bc2a2
reinich <reinich@outlook.com>
2023-04-17 17:17:21 +0300

    Server

---
+++ Семинары/Семинар 5/Task1/client.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Stream - поток, IFAM - ip4
+client.connect(("192.168.1.200", 55555))
+
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('utf-8'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+# 46.149.77.240 - сервер Романа
+# netstat -n командная строка для просмотра сообщений
